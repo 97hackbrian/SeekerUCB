f39327042de9ae60cf307a45de9a3a33aba6d384
97hackbrian <brayandurantoconas@gmail.com>
2023-12-05 16:49:48 -0400

    ros2

---
+++ Codes/2024/ros2_mpu6050_driver2/build/mpu6050driver/CMakeFiles/mpu6050driver.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/ros2_mpu6050_driver/src/mpu6050driver.cpp" "CMakeFiles/mpu6050driver.dir/src/mpu6050driver.cpp.o" "gcc" "CMakeFiles/mpu6050driver.dir/src/mpu6050driver.cpp.o.d"
+  "/home/ubuntu/ros2_mpu6050_driver/src/mpu6050sensor.cpp" "CMakeFiles/mpu6050driver.dir/src/mpu6050sensor.cpp.o" "gcc" "CMakeFiles/mpu6050driver.dir/src/mpu6050sensor.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Codes/2024/ros2_mpu6050_driver2/build/mpu6050driver/CMakeFiles/mpu6050driver.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mpu6050driver.
+# This may be replaced when dependencies are built.
